466a5937361ac2f776a0b4caf82bcd3c639765e9
gamped <mgampe17@student.aau.dk>
2020-05-20 11:11:30 +0000

    Changed from skipÃ_rnn to hid_skip_rnn param

--- backendFunctions.py
+++ backendFunctions.py
@@ -80,7 +80,10 @@ def HandleRenderPost(args):
         predictParams = MakePredictParams(args)
         url = str(noGithub["predictURL"]) + str(ConvertArgsToParams(predictParams))
         predictReq = requests.get(url)
-        predictDict = json.loads(predictReq.text)
+        try:
+            predictDict = json.loads(predictReq.text)
+        except ValueError as e:
+            return ReturnErrorResponse(e)
         predictID = re.sub("[^0-9a-zA-Z_\- ]", '', predictDict["predictid"])
         if not ValidateStringNoSymbol(predictID):
             return ReturnErrorResponse("Failed in predict stage: Invalid predictID")
@@ -210,7 +213,7 @@ def MakeRegisterParams(args):
     trainParams["hid_rnn"] = args["hid_rnn"]
     trainParams["window_rnn"] = args["window_rnn"]
     trainParams["windows_hw"] = args["windows_hw"]
-    trainParams["skip_rnn"] = args["skip_rnn"]
+    trainParams["hid_skip_rnn"] = args["hid_skip_rnn"]
     return trainParams
 
 # Take out the params needed for /predict
@@ -230,7 +233,7 @@ def FillPresetValues(args, presetName):
     args["hid_rnn"] = presetArr[3]
     args["window_rnn"] = presetArr[4]
     args["windows_hw"] = presetArr[5]
-    args["skip_rnn"] = presetArr[6]
+    args["hid_skip_rnn"] = presetArr[6]
     return args
 
 # Make a build ID HTML chart
@@ -288,7 +291,7 @@ def ValidationOfRenderArgs(args):
             checks.append(ValidateRenderNumber(args["hid_rnn"]))
             checks.append(ValidateRenderNumber(args["window_rnn"]))
             checks.append(ValidateRenderNumber(args["windows_hw"]))
-            checks.append(ValidateRenderNumber(args["skip_rnn"]))
+            checks.append(ValidateRenderNumber(args["hid_skip_rnn"]))
     
     if args["option"] == "tp":
         checks.append(ValidateStringNoSymbol(args["file_settings"]))
--- service.py
+++ service.py
@@ -62,7 +62,7 @@ class Render(Resource):
         parser.add_argument("hid_rnn")
         parser.add_argument("window_rnn")
         parser.add_argument("windows_hw")
-        parser.add_argument("skip_rnn")
+        parser.add_argument("hid_skip_rnn")
         args = parser.parse_args()
         renderHTML = HandleRenderPost(args)
 
@@ -148,7 +148,7 @@ class Combined(Resource):
         parser.add_argument("hid_rnn")
         parser.add_argument("window_rnn")
         parser.add_argument("windows_hw")
-        parser.add_argument("skip_rnn")
+        parser.add_argument("hid_skip_rnn")
         args = parser.parse_args()
         renderHTML = HandleRenderPost(args)
 
